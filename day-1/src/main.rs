c36bf61e543f536557fc376571e83807561a98d5
Jamin DeMattos <jaydemattos@gmail.com>
2023-12-13 18:52:51 -0800

    refactor

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,32 +1,33 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
-
-fn main() -> Result<(), Error> {
-    let input = File::open("input.txt")?;
-    let reader = BufReader::new(input);
-
-    let mut sum: usize = 0;
-    for line in reader.lines() {
-        sum += extract_outer_digits(line?.as_bytes());
-    }
+fn main() {
+    let sum = process_input();
 
     println!("{}", sum);
-
-    Ok(())
 }
 
-fn extract_outer_digits(bytes: &[u8]) -> usize {
-    let first_digit = bytes.iter().find_map(|&byte| digit(byte)).unwrap();
-    // NOTE: the DoubleEndedIterator trait allows us to iterate in reverse efficiently via rev(). this is pretty unusual
-    // to see in a programming language.
-    let last_digit = bytes.iter().rev().find_map(|&byte| digit(byte)).unwrap();
+fn process_input() -> usize {
+    let input = include_bytes!("../input.txt");
+    let lines = input
+        .split(|byte| *byte == b'\n')
+        .filter(|line| !line.is_empty());
+
+    let sum = lines
+        .map(|line| extract_outer_digits(line))
+        .sum();
+
+    sum
+}
+
+fn extract_outer_digits(line: &[u8]) -> usize {
+    let first_digit = line.iter().find_map(|byte| digit(byte)).unwrap();
+    // NOTE: the DoubleEndedIterator trait allows us to iterate in reverse efficiently via rev(). this is pretty unusual to see in a programming language.
+    let last_digit = line.iter().rev().find_map(|byte| digit(byte)).unwrap();
 
     10 * first_digit + last_digit
 }
 
-fn digit(byte: u8) -> Option<usize> {
+fn digit(byte: &u8) -> Option<usize> {
     match byte {
-        b'1'..=b'9' => Some((byte - b'0') as usize),
+        b'1'..=b'9' => Some((byte - b'0').into()),
         _ => None,
     }
 }
@@ -34,7 +35,6 @@ fn digit(byte: u8) -> Option<usize> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::str;
 
     #[test]
     fn test_extract_outer_digits() {
@@ -44,9 +44,17 @@ mod tests {
         assert_extract_outer_digits(b"treb7uchet", 77);
     }
 
+    #[test]
+    fn test_process_input() {
+        let actual = process_input();
+        let expected = 55_208;
+
+        assert_eq!(actual, expected, "expected {}, actual {}", expected, actual);
+    }
+
     fn assert_extract_outer_digits(input: &[u8], expected: usize) {
         let actual = extract_outer_digits(input);
-        let input_str = str::from_utf8(input).unwrap();
+        let input_str = std::str::from_utf8(input).unwrap();
         assert_eq!(
             actual, expected,
             "expected {} from {}, actual {}",
